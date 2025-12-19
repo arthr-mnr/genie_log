cf826b2bcbcc5ca62fd4efe9ae89092fed4d84ae
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 11:32:00 +0100

    TDD - 2eme test de vérification du nombre de caractères du mot de passe

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -2,6 +2,8 @@ const validatePassword = require('./passwordValidator');
 
 test("password w/ less than 8 characters should be invalid", () => {
     const checkLength1 = "abcdefghij"
+    const checkLength2 = "foo"
     
     expect(validatePassword(checkLength1)).toBe(true);
+    expect(validatePassword(checkLength1)).toBe(false);
 });
