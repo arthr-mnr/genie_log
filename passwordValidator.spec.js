285d0484fa820e272af9f4965de9f2ca9b6b4180
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 11:40:27 +0100

    TDD - 1er test de vérification de la présence d'un chiffre

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -7,3 +7,10 @@ test("password w/ less than 8 characters should be invalid", () => {
     expect(validatePassword(checkLength1)).toBe(true);
     expect(validatePassword(checkLength2)).toBe(false);
 });
+
+
+test("password w/out a number should be invalid", () => {
+    const checkLength1 = "abcdefghij123"
+    
+    expect(validatePassword(checkLength1)).toBe(true);
+});
