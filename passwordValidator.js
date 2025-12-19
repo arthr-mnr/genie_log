628e4e79b8e70c6c559120b5b3ddddc123e0d6fe
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 10:50:25 +0100

    Ajout de la vérification de la présence d'une lettre dans le mot de passe
    Modification de la variable checkNumber -> checkLetter dans le test

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,8 +1,9 @@
 function validatePassword(password) {
     const hasMinLength = password.length > 7;
     const hasNumber = [...password].some(char => char >= '0' && char <= '9');
+    const hasLetter = [...password].some(char => (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z'))
 
-    return hasMinLength && hasNumber;
+    return hasMinLength && hasNumber && hasLetter;
 }
 
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -16,3 +16,12 @@ test("password w/out a number should be invalid", () => {
   expect(validatePassword(checkNumber1)).toBe(true);
   expect(validatePassword(checkNumber2)).toBe(false);
 });
+
+
+const checkLetter1 = "123456789a"
+const checkLetter2 = "123456789"
+
+test("password w/out a letter should be invalid", () => {
+  expect(validatePassword(checkLetter1)).toBe(true);
+  expect(validatePassword(checkLetter2)).toBe(false);
+});
