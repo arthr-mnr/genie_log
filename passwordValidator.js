ddb98e6c14aa76df1404a3c1b3600e106d7fe623
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 11:51:29 +0100

    Ajout de la possibilité de passer la longueur minimale en paramètre

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,8 +1,9 @@
-function validatePassword(password) {
+function validatePassword(password, minLength=8) {
+    const hasLength = password.length > minLength
     const hasNumber = [...password].some(char => char >= '0' && char <= '9')
     const hasLetter = [...password].some(char => char >= 'a' && char <= 'z')
     
-    return password.length > 7 && hasNumber && hasLetter
+    return hasLength && hasNumber && hasLetter
 }
 
 module.exports = validatePassword;
