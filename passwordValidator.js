fce5ad8a33f84e74511915de8f8d0f7624ecffa7
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 11:48:35 +0100

    TDD - Ajout de la condition dans passwordValidator pour le 2eme test de vérification de la présence d'une lettre

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,7 +1,8 @@
 function validatePassword(password) {
     const hasNumber = [...password].some(char => char >= '0' && char <= '9')
+    const hasLetter = [...password].some(char => char >= 'a' && char <= 'z')
     
-    return password.length > 7 && hasNumber
+    return password.length > 7 && hasNumber && hasLetter
 }
 
 module.exports = validatePassword;
