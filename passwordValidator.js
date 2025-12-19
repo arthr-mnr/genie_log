18c78330edcd4b36322e7bf2792def809127b173
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 11:31:06 +0100

    TDD - 1er test de vérification du nombre de caractères du mot de passe

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,9 +1,5 @@
 function validatePassword(password) {
-    const hasMinLength = password.length > 7;
-    const hasNumber = [...password].some(char => char >= '0' && char <= '9');
-    const hasLetter = [...password].some(char => (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z'))
-
-    return hasMinLength && hasNumber && hasLetter;
+    return true
 }
 
 module.exports = validatePassword;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,24 +1,7 @@
 const validatePassword = require('./passwordValidator');
 
-test("password w/ less than 8c should be invalid", () => {
-    const checkLength1 = "password9"
-    const checkLength2 = "azert1"
+test("password w/ less than 8 characters should be invalid", () => {
+    const checkLength1 = "abcdefghij"
+    
     expect(validatePassword(checkLength1)).toBe(true);
-    expect(validatePassword(checkLength2)).toBe(false);
-});
-
-
-test("password w/out a number should be invalid", () => {
-    const checkNumber1 = "azerty12345"
-    const checkNumber2 = "azertyazerty"
-    expect(validatePassword(checkNumber1)).toBe(true);
-    expect(validatePassword(checkNumber2)).toBe(false);
-});
-
-
-test("password w/out a letter should be invalid", () => {
-    const checkLetter1 = "123456789a"
-    const checkLetter2 = "123456789"
-    expect(validatePassword(checkLetter1)).toBe(true);
-    expect(validatePassword(checkLetter2)).toBe(false);
 });
