c2d2d958db7a1aacd7792761f9ee3cac4c0911b0
Arthur MEUNIER <arthur.meunier@icloud.com>
2025-12-19 10:03:54 +0100

    Modification du message de test de longueur

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -3,7 +3,7 @@ const validatePassword = require('./passwordValidator');
 const checkLength1 = "password"
 const checkLength2 = "azerty"
 
-test("Check if the password's length is at least 8", () => {
+test("password w/ less than 8c should be invalid", () => {
   expect(validatePassword(checkLength1)).toBe(true);
   expect(validatePassword(checkLength2)).toBe(false);
 });
