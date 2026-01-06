d06549d21f60bc7d74a381286e6e9e923e4ef551
Arthur MEUNIER <arthur.meunier@icloud.com>
2026-01-06 17:14:53 +0100

    Création d'une BDD dédiée aux query

--- cqrs/accountCommand.mjs
+++ cqrs/accountCommand.mjs
@@ -1,12 +1,18 @@
 import { Account } from "./account.mjs";
 import { accountCommandDAO } from "./accountCommandDAO.mjs";
 import { accountQueryDAO } from "./accountQueryDAO.mjs";
-import { ACCOUNT_LIST } from "./database.mjs";
+import {queryDatabase} from "./queryDatabase.mjs"
 
 export const accountCommand = {
     addAccount(lastName, firstName) {
         const account = new Account(null, lastName, firstName, null);
         accountCommandDAO.insertAccount(account);
+        queryDatabase.accountSummaryList.push({
+            id: account.id,
+            lastName: account.lastName,
+            firstName: account.firstName
+        });
+        console.log(queryDatabase.accountSummaryList)
     },
     saveAccount(id, lastName, firstName) {
         let account = accountQueryDAO.restore(id)
