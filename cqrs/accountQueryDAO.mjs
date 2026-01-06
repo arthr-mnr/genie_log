1bcc3be4d41282cc20a80d9f23584fd2e8bd2a99
Arthur MEUNIER <arthur.meunier@icloud.com>
2026-01-06 17:53:44 +0100

    Modification de retrieveAccount pour utiliser le cache

--- cqrs/accountQueryDAO.mjs
+++ cqrs/accountQueryDAO.mjs
@@ -1,4 +1,5 @@
 import { Account } from "./account.mjs";
+import { accountCache } from "./cache.mjs";
 import { ACCOUNT_LIST } from "./database.mjs";
 import { queryDatabase } from "./queryDatabase.mjs";
 
@@ -7,10 +8,11 @@ export const accountQueryDAO = {
         return queryDatabase.accountSummaryList;
     },
     retrieveAccount(id) {
-        const index = ACCOUNT_LIST.findIndex(a => a.id === id);
+        /* const index = ACCOUNT_LIST.findIndex(a => a.id === id);
         let account = ACCOUNT_LIST[index];
         let res = {id: account.id, name: account.lastName + ' ' + account.firstName}
-        return res
+        return res */
+        return accountCache[id]
     },
     restore(id) {
         let account = ACCOUNT_LIST.find(a => a.id == id)
